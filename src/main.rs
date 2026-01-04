8b86a7a247711d374e20580dfda1c2fff54d03ff
theun1c <belovdima1122@yandex.ru>
2026-01-04 17:17:43 +0300

    reading rust book and practice before project start

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use std::{cmp::Ordering, io};
+
+use rand::{ random_range };
+
 fn main(){
-    println!("Hello, world!");
-}
+
+    let secret_number= random_range(1..=100);
+
+    println!("enter guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("error while reading");
+
+    let guess: u32 = guess.trim().parse().expect("converting error");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("to small"),
+        Ordering::Equal => println!("u win"),
+        Ordering::Greater => println!("to big"),
+    }
+    
+}   
 

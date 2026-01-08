c7493d5d0b447300dfd7264bc744a57f5d275ae4
theun1c <belovdima1122@yandex.ru>
2026-01-08 08:49:13 +0300

    commit before new branch create - clean repo

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,6 @@ use rand::random_range;
 
 fn main(){
 
-
-    let secret_number = random_range(1..=100);
-
-    loop{
-        println!("enter num: ");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("reading error");
-
-        let guess: i32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("ure guess: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Equal => {
-                println!("u win");
-                break;
-            },
-            Ordering::Greater => println!("big"),
-            Ordering::Less => println!("small"),
-        }
-    }
+    
 }   
 
